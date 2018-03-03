52d2b0c3242ab9cc91726d59c1f76fcfe9988038
Nathan Smith <nathansmithdeveloper@gmail.com>
2018-03-03 22:15:45 +1300

    blog views

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,4 +1,17 @@
 @extends('layout.main')
     @section('content')
-        <p>Blog page</p>
+        <h1>{{ config('blog.title') }}</h1>
+        <h2>Page {{ $posts->currentPage() }} of {{ $posts->lastPage() }}</h2>
+        <section>
+            @foreach($posts as $post)
+            <article>
+                <h3><a href="/blog/{{ $post->slug }}">{{ $post->title }}</a></h3>
+                <p><strong> {{ $post->published_at->format('jS M Y') }} at {{ $post->published_at->format('g:ia') }} </strong></p>
+                <div>
+                    {{ str_limit($post->content) }}
+                </div>
+            @endforeach
+            </article>
+        </section>
+        {{!! $posts->render() !!}}
     @stop
---
+++ resources/views/blog/single.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+    @section('content')
+        <h1>{{ $post->title }}</h1>
+        <p><strong> {{ $post->published_at->format('jS M Y') }} </strong></p>
+        <div>
+            {!! nl2br($post->content) !!}
+        </div>
+    @stop   
